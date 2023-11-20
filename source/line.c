a78a235444ef1b1a475e0fb2d54406801f8bb5f8
alope107 <auberonlopez@gmail.com>
2023-11-20 15:50:30 -0800

    Make trippy visual.
    - Fill up the full screen with some fun patterns.

--- source/line.c
+++ source/line.c
@@ -5,10 +5,12 @@
 int main () {
     REG_DISPCNT = DCNT_MODE3 | DCNT_BG2;
     
-    unsigned short x;
-    for(x = 0; x < SCREEN_WIDTH; x++){
-        m3_plot(x, SCREEN_HEIGHT >> 1, x << 10);
-    };
+    unsigned short x, y;
+    for (y = 0; y < SCREEN_HEIGHT; y++) {
+        for(x = 0; x < SCREEN_WIDTH; x++){
+            m3_plot(x, y, x*x + y*y);
+        };
+    }
 
     return 0;
 }
